11720e9c54a8aa232b7d62a750fa178c65b74990
Aron Strandberg <aron@aronstrandberg.com>
2018-07-20 15:36:19 +0200

    Make ActiveStorage::BaseController inherit from ActionController::API

--- activestorage/app/controllers/active_storage/base_controller.rb
+++ activestorage/app/controllers/active_storage/base_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 # The base controller for all ActiveStorage controllers.
-class ActiveStorage::BaseController < ActionController::Base
-  protect_from_forgery with: :exception
+class ActiveStorage::BaseController < ActionController::API
 
   before_action do
     ActiveStorage::Current.host = request.base_url
